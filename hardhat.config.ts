7a659fe26f8a3da19b1af74cf8f81347981fb39d
Arjun Raja Yogidas <yogidas.a@northeastern.edu>
2023-04-05 18:07:08 -0400

    deployment to test net

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+
+const ALCHEMY_API_KEY = "";
+
+const SEPOLIA_PRIVATE_KEY = "";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
